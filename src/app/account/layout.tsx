ae511ec9f3caeb1ab61f0de48aff14e14030b5ee
Aero Larson <aero.larson@gmail.com>
2025-12-04 11:31:33 -0800

    Add layout to force dynamic rendering for account route

---
+++ src/app/account/layout.tsx
@@ -0,0 +1,12 @@
+// Force dynamic rendering for account page
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+export default function AccountLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
+
